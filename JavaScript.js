a49f3614b7ac6f297ab4aec71f56eee53f0b59c6
kdaon07 <kdaon07@gmail.com>
2023-06-29 04:10:40 +0900

    test commit

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+function filter() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("listInner");
+
+    for (let i = 0; i < listInner.length; i++) {
+      na = listInner[i].getElementsByClassName("na");
+      number = listInner[i].getElementsByClassName("number");
+      if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+      number[0].innerHTML.toLowerCase().indexOf(search) != -1
+      ) {
+        listInner[i].style.display = "flex"
+      } else {
+        listInner[i].style.display = "none"
+      }
+    }
+  }
