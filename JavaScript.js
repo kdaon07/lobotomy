e007d073a5e4a57b056076d1ef4ce4890afb4a1f
kdaon07 <kdaon07@gmail.com>
2023-06-30 07:43:30 +0900

    commit

--- JavaScript.js
+++ JavaScript.js
@@ -3,14 +3,14 @@ function filter() {
     let listInner = document.getElementsByClassName("listInner");
 
     for (let i = 0; i < listInner.length; i++) {
-      na = listInner[i].getElementsByClassName("na");
-      number = listInner[i].getElementsByClassName("number");
-      if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
-      number[0].innerHTML.toLowerCase().indexOf(search) != -1
-      ) {
-        listInner[i].style.display = "flex"
-      } else {
-        listInner[i].style.display = "none"
-      }
+        na = listInner[i].getElementsByClassName("na");
+        number = listInner[i].getElementsByClassName("number");
+        if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+            number[0].innerHTML.toLowerCase().indexOf(search) != -1
+        ) {
+            listInner[i].style.display = "flex";
+        } else {
+            listInner[i].style.display = "none";
+        }
     }
-  }
+}
