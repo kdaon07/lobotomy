fe2a699e9952ac94e79f49dfe3c909cf6b911876
kdaon07 <kdaon07@gmail.com>
2023-07-12 15:34:13 +0900

    50%

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,12 @@
+function don() {
+    alert("만지지마");
+    document.getElementById("container").style.display = "none";
+    document.getElementById("hiddenimg").style.display = "flex";
+}
+
 function filter() {
     let search = document.getElementById("search").value.toLowerCase();
-    let listInner = document.getElementsByClassName("listInner");
+    let listInner = document.getElementsByClassName("AL");
 
     for (let i = 0; i < listInner.length; i++) {
         na = listInner[i].getElementsByClassName("na");
@@ -8,9 +14,250 @@ function filter() {
         if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
             number[0].innerHTML.toLowerCase().indexOf(search) != -1
         ) {
-            listInner[i].style.display = "flex";
+            if (btA == 1)
+                listInner[i].style.display = "flex";
+            filterW();
+        } else {
+            listInner[i].style.display = "none";
+            filterW();
+        }
+    }
+}
+
+function filterW() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("WA");
+
+    for (let i = 0; i < listInner.length; i++) {
+        na = listInner[i].getElementsByClassName("na");
+        number = listInner[i].getElementsByClassName("number");
+        if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+            number[0].innerHTML.toLowerCase().indexOf(search) != -1
+        ) {
+            if (btW == 1)
+                listInner[i].style.display = "flex";
+                filterH();
+        } else {
+            listInner[i].style.display = "none";
+            filterH();
+        }
+    }
+}
+
+function filterH() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("HE");
+
+    for (let i = 0; i < listInner.length; i++) {
+        na = listInner[i].getElementsByClassName("na");
+        number = listInner[i].getElementsByClassName("number");
+        if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+            number[0].innerHTML.toLowerCase().indexOf(search) != -1
+        ) {
+            if (btW == 1)
+                listInner[i].style.display = "flex";
+                filterT();
+        } else {
+            listInner[i].style.display = "none";
+            filterT();
+        }
+    }
+}
+
+function filterT() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("TE");
+
+    for (let i = 0; i < listInner.length; i++) {
+        na = listInner[i].getElementsByClassName("na");
+        number = listInner[i].getElementsByClassName("number");
+        if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+            number[0].innerHTML.toLowerCase().indexOf(search) != -1
+        ) {
+            if (btW == 1)
+                listInner[i].style.display = "flex";
+                filterZ();
+        } else {
+            listInner[i].style.display = "none";
+            filterZ();
+        }
+    }
+}
+
+function filterZ() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let listInner = document.getElementsByClassName("ZA");
+
+    for (let i = 0; i < listInner.length; i++) {
+        na = listInner[i].getElementsByClassName("na");
+        number = listInner[i].getElementsByClassName("number");
+        if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+            number[0].innerHTML.toLowerCase().indexOf(search) != -1
+        ) {
+            if (btW == 1)
+                listInner[i].style.display = "flex";
         } else {
             listInner[i].style.display = "none";
         }
     }
+}
+btA = 1;
+btW = 1;
+btH = 1;
+btT = 1;
+btZ = 1;
+function fA() {
+    if (btA == 1) {
+        btA = 0;
+        let listInner = document.getElementsByClassName("AL");
+
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            listInner[i].style.display = "none";
+        }
+
+    }
+    else {
+        btA = 1;
+        let search = document.getElementById("search").value.toLowerCase();
+        let listInner = document.getElementsByClassName("AL");
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+                number[0].innerHTML.toLowerCase().indexOf(search) != -1
+            )
+            listInner[i].style.display = "flex";
+
+        }
+    }
+}
+//줄확인----------------------------------------------------------------------------------------------------
+function fW() {
+    if (btW == 1) {
+        btW = 0;
+        let listInner = document.getElementsByClassName("WA");
+
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            listInner[i].style.display = "none";
+        }
+    }
+    else {
+        btW = 1;
+        let search = document.getElementById("search").value.toLowerCase();
+        let listInner = document.getElementsByClassName("WA");
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+                number[0].innerHTML.toLowerCase().indexOf(search) != -1
+            )
+                listInner[i].style.display = "flex";
+
+        }
+    }
+}
+//줄확인----------------------------------------------------------------------------------------------------
+function fH() {
+    if (btH == 1) {
+        btH = 0;
+        let listInner = document.getElementsByClassName("HE");
+
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            listInner[i].style.display = "none";
+        }
+    }
+    else {
+        btH = 1;
+        let search = document.getElementById("search").value.toLowerCase();
+        let listInner = document.getElementsByClassName("HE");
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+                number[0].innerHTML.toLowerCase().indexOf(search) != -1
+            )
+                listInner[i].style.display = "flex";
+
+        }
+    }
+}
+//줄확인----------------------------------------------------------------------------------------------------
+function fT() {
+    if (btT == 1) {
+        btT = 0;
+        let listInner = document.getElementsByClassName("TE");
+
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            listInner[i].style.display = "none";
+        }
+    }
+    else {
+        btT = 1;
+        let search = document.getElementById("search").value.toLowerCase();
+        let listInner = document.getElementsByClassName("TE");
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+                number[0].innerHTML.toLowerCase().indexOf(search) != -1
+            )
+                listInner[i].style.display = "flex";
+
+        }
+    }
+}
+//줄확인----------------------------------------------------------------------------------------------------
+function fZ() {
+    if (btZ == 1) {
+        btZ = 0;
+        let listInner = document.getElementsByClassName("ZA");
+
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            listInner[i].style.display = "none";
+        }
+    }
+    else {
+        btZ = 1;
+        let search = document.getElementById("search").value.toLowerCase();
+        let listInner = document.getElementsByClassName("ZA");
+        for (let i = 0; i < listInner.length; i++) {
+            na = listInner[i].getElementsByClassName("na");
+            number = listInner[i].getElementsByClassName("number");
+            if (na[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+                number[0].innerHTML.toLowerCase().indexOf(search) != -1
+            )
+                listInner[i].style.display = "flex";
+
+        }
+    }
+}
+
+
+
+function nt() {
+    let text = document.getElementById("nothing");
+    let textInner = document.getElementsByClassName("textInner");
+    for (let i = 0; i < textInner.length; i++) {
+        textInner[i].style.display = "none";
+    }
+    text.style.display = "flex";
+}
+
+function so() {
+    let text = document.getElementById("silent");
+    let textInner = document.getElementsByClassName("textInner");
+    for (let i = 0; i < textInner.length; i++) {
+        textInner[i].style.display = "none";
+    }
+    text.style.display = "flex";
 }
